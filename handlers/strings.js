fca9b911355f90104ab58381c9b6fdb3f44c8f55
Edward Jones <throne3d@gmail.com>
2018-07-30 21:58:47 +0000

    Make website, google account, and bot name configurable in strings

--- handlers/strings.js
+++ handlers/strings.js
@@ -1,41 +1,44 @@
+const website = null; // "http://niles.seanecoffey.com/"
+const websiteSetup = null; // "http://niles.seanecoffey.com/setup"
+const account = null; // "niles-291@niles-169605.iam.gserviceaccount.com"
+const botName = "the bot";
+
 const USAGE = "```\
-Niles Usage\n\
+Usage\n\
 ---------------------------\n\
 !display             -  Display your calendar\n\
 !update / !sync      -  Update the calendar\n\
 !create / !scrim     -  Create events using GCal's default interpreter. Example format: !scrim xeno June 5 8pm - 9pm\n\
 !delete              -  Delete an event. Format: !delete <day> <starttime>\n\
 !clean / !purge      -  Delete messages in current channel. Format: either !clean or !clean <number>\n\
-!stats / !info       -  Display a list of statistics and information about the Niles bot\n\
-!invite              -  Get an invite link for Niles to join your server!\n\
-!setup               -  Get details on how to set up Niles\n\
+!stats / !info       -  Display a list of statistics and information about " + botName + "\n\
+!invite              -  Get an invite link for " + botName + " to join your server!\n\
+!setup               -  Get details on how to set up " + botName + "\n\
 !id                  -  Set the Google calendar ID for the guild\n\
 !tz                  -  Set the timezone for the guild\n\
-!prefix              -  View or change the prefix for Niles\n\
+!prefix              -  View or change the prefix for " + botName + "\n\
 !help                -  Display this message\n\
-```\n\
-Visit http://niles.seanecoffey.com for more info.";
+```" + (website ? `\nVisit ${website} for more info.` : "");
 
 const USAGE_SETUP = "```\
-Niles Usage - SETUP MODE\n\
+Usage - SETUP MODE\n\
 ---------------------------\n\
 NOTE: ALL COMMANDS BECOME AVAILABLE AFTER SETUP IS COMPLETE\n\
-!setup               -  Get details on how to set up Niles\n\
+!setup               -  Get details on how to set up " + botName + "\n\
 !id                  -  Set the Google calendar ID for the guild\n\
 !tz                  -  Set the timezone for the guild\n\
-!prefix              -  View or change the prefix for Niles\n\
+!prefix              -  View or change the prefix for " + botName + "\n\
 !help                -  Display this message\n\
-```\n\
-Visit http://niles.seanecoffey.com/setup for more info.";
+```" + (websiteSetup ? `\nVisit ${websiteSetup} for more info.` : "");
 
 const NOTIFICATION_SETUP = "\
-Hi! Let's get me setup for use in this Discord. The steps are outlined below, but for a detailed setup guide, visit <http://niles.seanecoffey.com/setup>.\n\
+Hi! Let's get me setup for use in this Discord. The steps are outlined below" + (websiteSetup ? `, but for a detailed setup guide, visit ${websiteSetup}.` : "") + "\n\
 \n\
-1. Invite `niles-291@niles-169605.iam.gserviceaccount.com` to 'Make changes to events' under the Permission Settings on the Google Calendar you want to use with Niles.\n\
+1. Invite " + (account ? `\`${account}\`` : "my google account") + " to 'Make changes to events' under the Permission Settings on the Google Calendar you want to use with " + botName + ".\n\
 2. Enter the Calendar ID of the calendar in Discord using the `!id` command, e.g. `!id 123abc@123abc.com`.\n\
 3. Enter the timezone you want to use in Discord with the `!tz` command, e.g. `!tz GMT+10:00`. (Note: it must be formatted like this, as an offset from GMT.)\n\
 \n\
-Niles should now be able to sync with your Google calendar and interact with on you on Discord. Try `!display` to get started!";
+I should now be able to sync with your Google calendar and interact with on you on Discord. Try `!display` to get started!";
 
 module.exports = {
     USAGE,
